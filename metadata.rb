ef678ec66ce8dacfbc50f2f6f9f37cf3800ef26f
robin <robin@ooyala.com>
2016-02-22 10:53:57 -0800

    add if check for issues,source url

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'jsirex@gmail.com'
 license          'Apache'
 description      'Installs/Configures simple-nexus. No less. No more.'
 long_description 'Installs/Configures simple-nexus. No less. No more.'
-issues_url       'https://github.com/jsirex/simple-nexus-cookbook/issues'
-source_url       'https://github.com/jsirex/simple-nexus-cookbook'
+issues_url       'https://github.com/jsirex/simple-nexus-cookbook/issues'  if defined? issues_url
+source_url       'https://github.com/jsirex/simple-nexus-cookbook'  if defined? source_url
 version          '0.1.0'
 
 supports 'debian'
